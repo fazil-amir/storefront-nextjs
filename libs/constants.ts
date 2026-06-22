9c010074e172ea8f9126d9d0377cbfe233e049c3
fazil-amir <webmasterfazil@gmail.com>
2026-06-23 01:17:41 +0530

    page size increased

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,3 +1,3 @@
-export const PAGE_SIZE = 12;
+export const PAGE_SIZE = 15;
 export const API_BASE = "https://dummyjson.com/products";
 export const SUMMARY_FIELDS = "id,title,price,rating,thumbnail";
